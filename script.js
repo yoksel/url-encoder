3a3894286bf0c0b5e7aa3e32117eaba26f7f44a1
yoksel <yoksel@yandex.ru>
2014-09-03 17:01:34 +0400

    Mimification

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ var resTar = doc.querySelector("#result");
 var resCssTar = doc.querySelector("#result-css");
 var resDemo = doc.querySelector("#demo");
 
+var expanders = doc.querySelectorAll(".expander");
+var expandedClass = "expanded";
+
+// Textarea Actions
+//----------------------------------------
+
 initTar.onchange = function() {
     resTar.value = escape(initTar.value);
 };
@@ -15,9 +21,24 @@ initTar.onkeyup = function() {
     var resultCss = 'background-image: url(data:image/svg+xml,' + escaped + ')';
     resCssTar.value = resultCss;
     resDemo.setAttribute("style", resultCss);
-
-    console.log(resDemo);
 };
+
+// Tabs Actions
+//----------------------------------------
+
+for (var i = 0; i < expanders.length; i++) {
+    var expander = expanders[i];
+
+    expander.onclick = function() {
+        var parent = this.parentNode;
+        var expanded = parent.querySelector("." + expandedClass);
+        expanded.classList.toggle("hidden");
+        this.classList.toggle("opened");
+    };
+}
+
+// Common
+//----------------------------------------
 
 function out(data) {
     console.log(data);
