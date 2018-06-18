dadb691197fff6165f310c33d935ed9c8b244734
yokselzok <yokselzok@gmail.com>
2018-06-18 20:21:54 +0300

    Add quotes switcher. Close #13

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var doc = document;
 
-var initTar = doc.querySelector( "#init" );
-var resTar = doc.querySelector( "#result" );
-var resCssTar = doc.querySelector( "#result-css" );
-var resDemo = doc.querySelector( "#demo" );
+var initTextarea = doc.querySelector( "#init" );
+var resultTextarea = doc.querySelector( "#result" );
+var resultCssTextarea = doc.querySelector( "#result-css" );
+var resultDemo = doc.querySelector( "#demo" );
 var demoWrapper = doc.querySelector( ".demo-wrapper" );
 var contrastButtons = doc.querySelectorAll( ".contrast-button" );
 var contrastButtonCurrent = null;
@@ -14,21 +14,37 @@ var expandedClass = "expanded";
 var demoContrastClass = "demo-contrast-on";
 var symbols = /[\r\n"%#()<>?\[\\\]^`{|}]/g;
 
+const quotesInputs = document.querySelectorAll('.options__input');
+let externalQuotesValue = document.querySelector('.options__input:checked').value;
+let quotes = getQuotes();
+
+const buttonExample = document.querySelector('.button-example');
+
+
 // Textarea Actions
 //----------------------------------------
 
-initTar.onchange = function() {
-    resTar.value = encodeSVG(initTar.value);
+initTextarea.onchange = function() {
+    resultTextarea.value = encodeSVG(initTextarea.value);
 };
 
-initTar.onkeyup = function() {
-    var namespaced = addNameSpace( initTar.value );
-    var escaped = encodeSVG( namespaced );
-    resTar.value = escaped;
-    var resultCss = 'background-image: url("data:image/svg+xml,' + escaped + '");';
-    resCssTar.value = resultCss;
-    resDemo.setAttribute( "style", resultCss );
+initTextarea.onkeyup = function() {
+    getResults();
 };
+
+function getResults() {
+    if(!initTextarea.value) {
+        return;
+    }
+
+    var namespaced = addNameSpace( initTextarea.value );
+    var escaped = encodeSVG( namespaced );
+    resultTextarea.value = escaped;
+    var resultCss = `background-image: url(${quotes.level1}data:image/svg+xml,${escaped}${quotes.level1});`;
+    resultCssTextarea.value = resultCss;
+    resultDemo.setAttribute( "style", resultCss );
+}
+
 
 // Tabs Actions
 //----------------------------------------
@@ -43,6 +59,35 @@ for (var i = 0; i < expanders.length; i++) {
         this.classList.toggle( "opened" );
     };
 }
+
+
+// Switch quotes
+//----------------------------------------
+
+quotesInputs.forEach(input => {
+   input.addEventListener('input', function () {
+        externalQuotesValue = this.value;
+        quotes = getQuotes();
+        getResults();
+    });
+});
+
+
+// Set example
+//----------------------------------------
+
+buttonExample.addEventListener('click', () => {
+    initTextarea.value = `<svg>
+  <circle r="50" cx="50" cy="50" fill="tomato"/>
+  <circle r="41" cx="47" cy="50" fill="orange"/>
+  <circle r="33" cx="48" cy="53" fill="gold"/>
+  <circle r="25" cx="49" cy="51" fill="yellowgreen"/>
+  <circle r="17" cx="52" cy="50" fill="lightseagreen"/>
+  <circle r="9" cx="55" cy="48" fill="teal"/>
+</svg>`;
+    getResults();
+})
+
 
 // Demo Background Switch
 //----------------------------------------
@@ -70,23 +115,25 @@ contrastButtons.forEach(button => {
     });
 });
 
+
 // Namespace
 //----------------------------------------
 
 function addNameSpace( data ) {
-    if ( data.indexOf( "http://www.w3.org/2000/svg" ) < 0 ) {
-        data = data.replace( /<svg/g, "<svg xmlns='http://www.w3.org/2000/svg'" );
+    if ( data.indexOf( 'http://www.w3.org/2000/svg' ) < 0 ) {
+        data = data.replace( /<svg/g, `<svg xmlns=${quotes.level2}http://www.w3.org/2000/svg${quotes.level2}` );
     }
 
     return data;
 }
+
 
 // Encoding
 //----------------------------------------
 
 function encodeSVG( data ) {
     // Use single quotes instead of double to avoid encoding.
-    if ( data.indexOf( '"' ) >= 0 ) {
+    if ( externalQuotesValue === 'double' && data.indexOf( '"' ) >= 0 ) {
         data = data.replace( /"/g, "'" );
     }
 
@@ -94,6 +141,20 @@ function encodeSVG( data ) {
     data = data.replace( /\s{2,}/g, " " );
 
     return data.replace( symbols, escape );
+}
+
+
+// Get quotes for levels
+//----------------------------------------
+
+function getQuotes() {
+    const double = `"`;
+    const single = `'`;
+
+    return {
+        level1: externalQuotesValue === 'double' ? double : single,
+        level2: externalQuotesValue === 'double' ? single : double
+    };
 }
 
 // Common
