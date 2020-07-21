b11f13f98baaa95d751aadb79b0a4c9df181c63c
yoksel <yokselzok@gmail.com>
2020-07-21 12:14:43 +0300

    Lint linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,18 @@ module.exports = {
     browser: true,
     es6: true
   },
-  extends: ['standard'],
+  extends: [`standard`],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    Atomics: `readonly`,
+    SharedArrayBuffer: `readonly`
   },
-  parser: 'babel-eslint',
+  parser: `babel-eslint`,
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2018
   },
   rules: {
-    semi: ['error', 'always'],
-    indent: ['error', 2],
-    quotes: [2, 'backtick']
+    semi: [`error`, `always`],
+    indent: [`error`, 2],
+    quotes: [2, `backtick`]
   }
 };
